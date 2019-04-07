e49a6faffa97cd3c4a3874e5d7c2b8d2ebb1b69a
feliciaalbertha <49154320+feliciaalbertha@users.noreply.github.com>
2019-04-07 11:53:35 +0700

    Add files via upload

---
+++ layout_manual_lib.h
@@ -0,0 +1,7 @@
+#include"layout_manual_lib.c"
+
+	void layout_manual();
+	int cekRC(int x1, int x2, int y1, int y2, int syarat);
+	int cekT(int x1, int x2,int x3, int y1, int y2, int y3, int syarat);
+	int cekPCB(int x, int y, int N, int M);
+	int cekKoordinat2 (int x1, int x2, int y1, int y2);
